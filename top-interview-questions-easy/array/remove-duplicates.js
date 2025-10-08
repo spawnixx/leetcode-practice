5a7eb3dfc991899f944dd45851a2aaf17391def6
Gabriel <Gabriel.thornburgh1@gmail.com>
2025-10-08 02:25:26 -0400

    Remove duplicates from sorted array Leetcode question

--- top-interview-questions-easy/array/remove-duplicates.js
+++ top-interview-questions-easy/array/remove-duplicates.js
@@ -1,2 +1,17 @@
-asdfhjkdasf;
-asd;
+
+const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+
+var removeDuplicates = function(nums) {
+    let unique = 0;
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i] !== nums[unique]){
+            unique ++;
+            nums[unique] = nums[i];
+        }
+    }
+    return(unique + 1);
+};
+
+k = removeDuplicates(nums);
+console.log(k);
+console.log(nums);
