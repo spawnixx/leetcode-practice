9dc08a0181b026539506bc6cd83be6bec5a64ba3
Gabriel <Gabriel.thornburgh1@gmail.com>
2025-10-08 14:05:58 -0400

    Attempt 1

---
+++ top-interview-questions-easy/array/rotate-array.js
@@ -0,0 +1,13 @@
+const nums = [1, 2, 3, 4, 5, 6, 7];
+const k = 3;
+
+var rotate = function (nums, k) {
+    nums.reverse();
+    for (let i = 0; i < nums.length; i++) {
+    if(i < k){
+        
+    }
+  }
+};
+// rotate(nums, k);
+// console.log(nums);
